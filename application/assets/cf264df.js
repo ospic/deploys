ad405dea35b95ff82f28b67aa3ea778e52ba45b2
elirehema <29277048+elirehema@users.noreply.github.com>
2021-03-30 18:21:00 +0000

    Deploying to master from @ ospic/webapp@5f1508240360617345eb24192ca8886134d2fa8c 🚀

---
+++ application/assets/cf264df.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[44],{832:function(n,t,e){"use strict";e.r(t);var c=e(8),l=e(13),r=e.n(l),o=e(262),component=Object(c.a)({},(function(){var n=this,t=n.$createElement,e=n._self._c||t;return e("v-card",{staticClass:"pa-1"},[e("h1",[n._v("Email configurations")])])}),[],!1,null,null,null);t.default=component.exports;r()(component,{VCard:o.a})}}]);
