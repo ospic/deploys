cc07861da3476455ad0495ddcba929e28998b674
elirehema <29277048+elirehema@users.noreply.github.com>
2021-06-04 11:14:06 +0000

    Deploying to master from @ ospic/webapp@8a9527ae4c08c9c85d16217bce29d3638214cb12 🚀

---
+++ application/assets/4186046.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[40],{704:function(n,e,t){"use strict";t.r(e);var c=t(35),component=Object(c.a)({},(function(){var n=this,e=n.$createElement;return(n._self._c||e)("h1",[n._v("Insurance")])}),[],!1,null,null,null);e.default=component.exports}}]);
