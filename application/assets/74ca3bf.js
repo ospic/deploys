3c627b4c7a4e0fa3da45644b0f7841958dbde6dc
elirehema <29277048+elirehema@users.noreply.github.com>
2021-07-12 17:08:04 +0000

    Deploying to master from @ ospic/webapp@88f234a394a2e7eb15b7a3dba3dc768d2b5b6e10 🚀

---
+++ application/assets/74ca3bf.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[73],{796:function(n,e,t){"use strict";t.r(e);var c={components:{pharmacy:t(777).default}},l=t(44),component=Object(l.a)(c,(function(){var n=this.$createElement;return(this._self._c||n)("pharmacy")}),[],!1,null,null,null);e.default=component.exports}}]);
